e75b66f57384dfc4477766c8e915ba5ac2056d8e
João Ferreira <jd.ferreira@ua.pt>
2025-04-09 00:23:48 +0100

    HW1: Início do projeto.

---
+++ HW1/MoliceiroU/src/main/java/com/example/MoliceiroU/MoliceiroUApplication.java
@@ -0,0 +1,13 @@
+package com.example.MoliceiroU;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoliceiroUApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoliceiroUApplication.class, args);
+	}
+
+}
---
+++ HW1/MoliceiroU/src/main/java/com/example/MoliceiroU/entity/Restaurant.java
@@ -0,0 +1,8 @@
+package com.example.MoliceiroU.entity;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Restaurant {
+
+}
